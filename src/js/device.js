e6bbca8ec588f02b95b3eefdb4e83c7235215bb0
Joel Cannon <joelcannon@mac.com>
2023-11-28 16:56:05 -0600

    add infoWindow

--- src/js/device.js
+++ src/js/device.js
@@ -27,30 +27,42 @@ async function initMap() {
   const locations = tempMarkers.map((item) => item.position);
 
   // Add some markers to the map.
-  const markers = locations.map((position, i) => {
+  //   const markers = locations.map((position, i) => {
+  const markers = tempMarkers.map((zip, i) => {
     const label = labels[i % labels.length];
     const pinGlyph = new google.maps.marker.PinElement({
       glyph: label,
       glyphColor: 'white',
     });
+
+    //  const testposition = { lat: zip.LAT, lng: zip.LON };
+    const testposition = zip.position;
+    console.log(testposition);
+
     const marker = new google.maps.marker.AdvancedMarkerElement({
-      position,
+      // position: { lat: +zip.LAT, lng: +zip.LON },
+      position: zip.position,
       content: pinGlyph.element,
     });
+
+    const contentString = `<div class='Zip Code'><b>${zip.PC}</b></div>
+    <div class='city'>${zip.PN}</div>
+    <div class='county'>
+    ${zip.AN2}, ${zip.AC1}
+    </div>`;
 
     // markers can only be keyboard focusable when they have click listeners
     // open info window when marker is clicked
     marker.addListener('click', () => {
-      infoWindow.setContent(position.lat + ', ' + position.lng);
+      // infoWindow.setContent(position.lat + ', ' + position.lng);
+      infoWindow.setContent(contentString);
       infoWindow.open(map, marker);
     });
     return marker;
   });
 
   // Add a marker clusterer to manage the markers.
-  new MarkerClusterer({ markers, map });
-
-  //   const markerCluster = new MarkerClusterer({ markers, map });
+  const markerCluster = new MarkerClusterer({ markers, map });
 }
 
 initMap();
--- src/js/externalServices.mjs
+++ src/js/externalServices.mjs
@@ -17,12 +17,32 @@ export function loadMarkersFromJsonFile() {
     });
 }
 
-export async function loadZipMarkersFromJsonFile() {
-  const response = await fetch('/json/csvjson.json');
+export function loadZipMarkersFromJsonFile() {
+  return fetch('/json/zip.json')
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then((data) => {
+      return data ? data : [];
+    })
+    .catch((error) => {
+      console.error(
+        'There has been a problem with your fetch operation:',
+        error
+      );
+    });
+}
+
+export async function xloadZipMarkersFromJsonFile() {
+  const response = await fetch('/json/zip.json');
   const data = await response.json();
   return data.map((item) => {
-    return {
-      position: { lat: item.LAT, lng: item.LON },
-    };
+    return data ? data : [];
+    // {
+    // position: { lat: item.LAT, lng: item.LON },
+    // };
   });
 }
