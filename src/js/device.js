5c4b719d4ef83f7c0d007b14ed3d3bc97593f04a
Joel Cannon <joelcannon@mac.com>
2023-12-12 23:54:38 -0600

    create device detail page

--- src/js/device.js
+++ src/js/device.js
@@ -1,80 +1,41 @@
-import { MarkerClusterer } from '@googlemaps/markerclusterer';
 import { loadDeviceMarkersFromJsonFile } from './externalServices.mjs';
-import { loadHeader } from './utils.mjs';
+import { loadHeader, getParam } from './utils.mjs';
 
 loadHeader('Device');
 
-/* global google */
-async function initMap() {
-  // Request needed libraries.
-  const { Map, InfoWindow } = await google.maps.importLibrary('maps');
-  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
-    'marker'
-  );
-  const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: { lat: 39.93459, lng: -93.924026 },
-    mapTypeId: 'satellite',
-    mapId: 'DEMO_MAP_ID',
-  });
-  const infoWindow = new google.maps.InfoWindow({
-    content: '',
-    disableAutoPan: true,
-  });
-  // Create an array of alphabetical characters used to label the markers.
-  // const labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// Fetch the JSON file
+fetch('/json/device.json')
+  .then((response) => response.json())
+  .then((data) => {
+    // Get the device id from the URL parameters
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
 
-  // Load locations from JSON file
-  const tempMarkers = await loadDeviceMarkersFromJsonFile();
-  const locations = tempMarkers.map((item) => item.position);
+    // Find the device with the matching id
+    // const foundDevice = data.find((device) => device.Id === id);
+    const foundDevice = data[id];
 
-  // Add some markers to the map.
-  const markers = tempMarkers.map((device, i) => {
-    // A marker using a Font Awesome icon for the glyph.
-    const icon = document.createElement('div');
-    icon.innerHTML = '<i class="fab fa-viacoin" aria-hidden="true"></i>';
-    const faPin = new google.maps.marker.PinElement({
-      glyph: icon,
-      glyphColor: 'white',
-      background: 'darkgreen',
-      borderColor: 'white',
-    });
+    // Display the device details
+    if (foundDevice) {
+      document.getElementById(
+        'device-name'
+      ).textContent = `${foundDevice.name}`;
+      document.getElementById('device-position').textContent = ` 
+      ${parseFloat(foundDevice.position.lat).toFixed(4)}° N, 
+      ${parseFloat(-foundDevice.position.lng).toFixed(4)}° W`;
 
-    const detail = document.createElement('div');
-
-    detail.className = 'detail';
-
-    if (device.status === 'Critical') {
-      detail.classList.add('critical');
-    } else if (device.status === 'Warning') {
-      detail.classList.add('warning');
+      document.getElementById(
+        'device-status'
+      ).textContent = `Status: ${foundDevice.status}`;
+      document.getElementById(
+        'device-temperature'
+      ).textContent = `Temp: ${foundDevice.temp}°F`;
+      document.getElementById(
+        'device-battery'
+      ).textContent = `Battery: ${foundDevice.battery}%`;
+      document.getElementById(
+        'device-voltage'
+      ).textContent = `Voltage: ${foundDevice.voltage} v`;
     }
-
-    detail.innerHTML =
-      '<i class="fab fa-viacoin icon-shadow" aria-hidden="true"></i>';
-
-    const marker = new google.maps.marker.AdvancedMarkerElement({
-      position: device.position,
-      content: detail,
-    });
-
-    const contentString = `<div class='d_name'><b>${device.name}</b></div>
-    <div class='d_status'>${device.status}</div>
-    <div class='d_stats'>
-    ${device.temp}°F, ${device.battery}%, ${device.voltage}v
-    </div>`;
-
-    // markers can only be keyboard focusable when they have click listeners
-    // open info window when marker is clicked
-    marker.addListener('click', () => {
-      infoWindow.setContent(contentString);
-      infoWindow.open(map, marker);
-    });
-    return marker;
-  });
-
-  // Add a marker clusterer to manage the markers.
-  const markerCluster = new MarkerClusterer({ markers, map });
-}
-
-initMap();
+  })
+  .catch((error) => console.error('Error:', error));
